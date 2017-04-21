a32e69cade83da19c9ddf02795cbce15f8a83410
RobotMermaid <ondinerangel@gmail.com>
2017-04-21 10:34:41 -0700

    first push

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("hi there Mermaid");
+console.error("oops!");
+console.dir({name: 'Ondine', status: 'awesome'});
+
+// Problem: We need a simple way to look at a user's badge count and JavaScript points
+// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+//COnnect to the api url (https://teamtreehouse.com/username.json)
+//Read the data
+//Parse the data
+//Print the data
