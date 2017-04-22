d1d204b8623796fd766bee9ddb2e2f35c929c622
RobotMermaid <ondinerangel@gmail.com>
2017-04-21 18:30:29 -0700

    response with parsed json started on errors

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-console.log("hi there Mermaid");
-console.error("oops!");
-console.dir({name: 'Ondine', status: 'awesome'});
-
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
-//COnnect to the api url (https://teamtreehouse.com/username.json)
-//Read the data
-//Parse the data
-//Print the data
+
+//requirs https module
+const https = require('https')
+
+
+//Function to print message to the console
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badges and ${points} points in JavaScript`;
+  console.log(message);
+}
+
+// printMessage("ondine", 100, 200000);
+
+
+//Connect to the api url  https://teamtreehouse.com/ondinemagalirangel.json
+function getProfile(username) {
+  const request = https.get(`https://teamtreehousecom/${username}.json`, response => {
+                            let body = "";
+                            // Read data in (string)
+                            // Parse the data
+                            // Print out the data we got from the api
+                            // console.dir(response);
+                            // console.log(response.statusCod);
+                            response.on('data', data => {
+                              body += data.toString();
+                            });
+                            response.on('end', () => {
+                              const profile = JSON.parse(body)
+                              printMessage(username, profile.badges.length, profile.points.JavaScript);
+                            });
+
+                          });
+  request.on('error', error => console.error(`Problem with the request: ${error.message}`));
+}
+// const users = ['ondinemagalirangel', 'chalkers'];
+const users = process.argv.slice(2);
+
+users.forEach(getProfile);
